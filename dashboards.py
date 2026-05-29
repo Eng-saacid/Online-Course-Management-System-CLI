b53fe8194d05aea3fac5cecf4df4b3bfc1f8865c
https://github.com/Eng-saaci <saacidsheikhcabdicaziiz@gmail.com>
2026-05-29 19:37:13 +0300

    Updated project with new features

--- dashboards.py
+++ dashboards.py
@@ -1,9 +1,10 @@
 
-from auth import register_menu
+
 from services.course_service import course_menu
 from services.grade_service import grade_menu
 from services.student_service import enrollment_menu, student_menu
 from services.teacher_service import teacher_menu
+from services.users_service import UserService
 
 # TODO: Admin Dashboard
 
@@ -20,7 +21,7 @@ def admin_menu(user):
 3. Courses
 4. Enrollment
 5. Grades
-6. Register User
+6. users
 7. Logout
 """)
 
@@ -42,7 +43,7 @@ def admin_menu(user):
             grade_menu()
 
         elif choice == "6":
-            register_menu(user)
+            user_menu()
 
         elif choice == "7":
             print("Logging out...")
@@ -120,3 +121,218 @@ def student_dashboard(user):
                 break
             else:
                 continue
+
+
+# =========================
+#       USER MENU
+# =========================
+
+def user_menu():
+
+    service = UserService()
+
+    while True:
+
+        print("""
+===== USER MANAGEMENT =====
+
+1. Register User
+2. View Users
+3. Search User
+4. Update User
+5. Delete User
+6. Back
+0. Exit
+""")
+
+        choice = input(
+            "Choose: "
+        ).strip()
+
+        # REGISTER USER
+        if choice == "1":
+
+            username = input(
+                "Username: "
+            ).strip()
+
+            password = input(
+                "Password: "
+            ).strip()
+
+            print("""
+1. Admin
+2. Teacher
+3. Student
+""")
+
+            role_choice = input(
+                "Choose Role: "
+            ).strip()
+
+            role = "student"
+
+            if role_choice == "1":
+                role = "admin"
+
+            elif role_choice == "2":
+                role = "teacher"
+
+            service.register_user(
+                username,
+                password,
+                role
+            )
+
+        # VIEW USERS
+        elif choice == "2":
+
+            users = service.get_users()
+
+            if not users:
+
+                print(
+                    "No users found"
+                )
+
+                continue
+
+            print(
+                "\n===== USERS LIST ====="
+            )
+
+            for index, user in enumerate(
+                users,
+                start=1
+            ):
+
+                print(
+                    f"{index}. "
+                    f"{user['username']} | "
+                    f"{user['role']}"
+                )
+
+        # SEARCH USER
+        elif choice == "3":
+
+            username = input(
+                "Enter Username: "
+            ).strip()
+
+            user = service.find_user(
+                username
+            )
+
+            if user:
+
+                print(
+                    f"""
+ID: {user['id']}
+Username: {user['username']}
+Role: {user['role']}
+"""
+                )
+
+            else:
+
+                print(
+                    "User not found"
+                )
+
+        # UPDATE USER
+        elif choice == "4":
+
+            username = input(
+                "Enter Username: "
+            ).strip()
+
+            user = service.find_user(
+                username
+            )
+
+            if not user:
+
+                print(
+                    "User not found"
+                )
+
+                continue
+
+            new_password = input(
+                "New Password: "
+            ).strip()
+
+            print("""
+1. Admin
+2. Teacher
+3. Student
+""")
+
+            role_choice = input(
+                "Choose New Role: "
+            ).strip()
+
+            new_role = "student"
+
+            if role_choice == "1":
+                new_role = "admin"
+
+            elif role_choice == "2":
+                new_role = "teacher"
+
+            if service.update_user(
+                username,
+                new_password,
+                new_role
+            ):
+
+                print(
+                    "User updated successfully"
+                )
+
+            else:
+
+                print(
+                    "Update failed"
+                )
+
+        # DELETE USER
+        elif choice == "5":
+
+            username = input(
+                "Enter Username: "
+            ).strip()
+
+            if service.delete_user(
+                username
+            ):
+
+                print(
+                    "User deleted successfully"
+                )
+
+            else:
+
+                print(
+                    "User not found"
+                )
+
+        # BACK
+        elif choice == "6":
+
+            break
+
+        # EXIT
+        elif choice == "0":
+
+            print(
+                "Exiting..."
+            )
+
+            exit()
+
+        else:
+
+            print(
+                "Invalid choice"
+            )
